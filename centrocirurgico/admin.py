1dd2ba82fba446020751e73d8fe32f99305a9d8e
Alexandre Oliveira <49107539+alexandre-ronald@users.noreply.github.com>
2026-09-11 08:20:43 -0300

    CCS-001: registrar tipos e motivos no admin

--- centrocirurgico/admin.py
+++ centrocirurgico/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MotivoSuspensao, TipoSuspensao
+
+
+@admin.register(TipoSuspensao)
+class TipoSuspensaoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "ordem", "ativo", "atualizado_em")
+    list_filter = ("ativo",)
+    search_fields = ("nome", "descricao")
+    ordering = ("ordem", "nome")
+
+
+@admin.register(MotivoSuspensao)
+class MotivoSuspensaoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "tipo", "ordem", "ativo", "atualizado_em")
+    list_filter = ("ativo", "tipo")
+    search_fields = ("nome", "descricao", "tipo__nome")
+    autocomplete_fields = ("tipo",)
+    ordering = ("tipo__ordem", "tipo__nome", "ordem", "nome")
